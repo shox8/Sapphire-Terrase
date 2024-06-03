f4b0bf39bce67dd7ef94e9a6646b9b34c55bf825
Shoxruh <138918930+Shox2008@users.noreply.github.com>
2024-06-03 17:48:50 +0500

    Navbar Done

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,6 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { Block } from "./style";
+import { Link } from "react-router-dom";
+import { GoArrowUpRight } from "react-icons/go";
+import { IoMdHeart } from "react-icons/io";
+import { FaGripLines } from "react-icons/fa6";
 
 export default function Navbar() {
-  return <Block>Navbar</Block>;
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Block open={open}>
+      <img src={require("../../assets/logo.png")} />
+      <div className="line">
+        <div className="links">
+          <Link to="/apartments">Apartments</Link>
+          <Link to="/gallery">Gallery</Link>
+          <Link to="/about">About Us</Link>
+          <Link to="/contacts">Contacts</Link>
+        </div>
+        <div className="socials">
+          <Link to="https://t.me" target="_blank">
+            Telegram
+          </Link>
+          <Link to="https://instagram.com" target="_blank">
+            Instagram
+          </Link>
+          <Link to="https://youtube.com" target="_blank">
+            Youtube
+          </Link>
+        </div>
+        <div className="services">
+          <Link to="/">
+            Request Call <GoArrowUpRight />
+          </Link>
+          <Link to="/">
+            Choose an apartment <GoArrowUpRight />
+          </Link>
+        </div>
+        <div className="languages">
+          <span>EN</span>
+          <span>UZ</span>
+        </div>
+      </div>
+      <div className="icons">
+        <IoMdHeart />
+        <FaGripLines onClick={() => setOpen(!open)} />
+      </div>
+    </Block>
+  );
 }
--- src/components/navbar/style.jsx
+++ src/components/navbar/style.jsx
@@ -1,3 +1,82 @@
 import styled from "styled-components";
 
-export const Block = styled.div``;
+export const Block = styled.div`
+  padding: 20px;
+  text-transform: uppercase;
+  background-color: #fff;
+  position: sticky;
+  top: 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: start;
+  .line {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: start;
+    margin-left: 20%;
+    margin-right: 40px;
+    a {
+      color: #2c2d2d;
+      white-space: nowrap;
+    }
+    .links,
+    .socials,
+    .services {
+      display: flex;
+      flex-direction: column;
+      gap: 5px;
+      font-size: 10px;
+    }
+    .links {
+      a {
+        text-decoration: none;
+      }
+    }
+    .languages {
+      display: flex;
+      gap: 15px;
+      font-size: 12px;
+      span {
+        cursor: pointer;
+      }
+      :nth-child(2) {
+        color: #b5b5b5;
+      }
+    }
+  }
+  .icons {
+    display: flex;
+    gap: 20px;
+    align-items: center;
+    height: 15px;
+    svg {
+      cursor: pointer;
+    }
+    :nth-child(2) {
+      font-size: 23px;
+    }
+  }
+  @media screen and (max-width: 700px) {
+    display: block;
+    height: ${(p) => (p.open ? "auto" : "70px")};
+    padding: 10px;
+    overflow: hidden;
+    img {
+      height: 30px;
+      margin-top: 10px;
+    }
+    .icons {
+      position: absolute;
+      top: calc(55px / 2);
+      right: 20px;
+    }
+    .line {
+      margin-top: 30px;
+      margin-inline: 0;
+      gap: 20px;
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
+`;
