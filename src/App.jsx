25aa13f18a313532ac62167ded9a0709983a4006
Shoxruh <138918930+Shox2008@users.noreply.github.com>
2024-06-03 22:34:36 +0500

    Footer Done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import Navbar from "./components/navbar";
+import Footer from "./components/footer";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </>
   );
 }
