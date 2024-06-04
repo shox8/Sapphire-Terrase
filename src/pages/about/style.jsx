a7689f5ef8abd8fde850587aa7c38ef23fb061b5
Shoxruh <138918930+Shox2008@users.noreply.github.com>
2024-06-04 07:55:40 +0500

    Gallery Done

--- src/pages/about/style.jsx
+++ src/pages/about/style.jsx
@@ -1,3 +1,22 @@
 import styled from "styled-components";
 
-export const Block = styled.div``;
+export const Block = styled.div`
+  padding-block: 20px;
+  padding-inline: 50px;
+  color: #2c2d2d;
+  hr {
+    margin-block: 40px;
+    border: 0.1px solid #d8d8d8;
+  }
+  .head-img {
+    width: 100%;
+  }
+  @media screen and (max-width: 700px) {
+    padding: 10px;
+    .head-img {
+      width: 100%;
+      height: calc(100dvh - 140px);
+      object-fit: cover;
+    }
+  }
+`;
